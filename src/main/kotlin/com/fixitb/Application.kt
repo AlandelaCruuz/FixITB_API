3a51c6c67f8f99185d134beeacc0ae44e2a155c2
Alan <alan.delacruz.7e6@itb.cat>
2024-04-15 16:47:12 +0200

    dropcolumn-classId-API

--- src/main/kotlin/com/fixitb/Application.kt
+++ src/main/kotlin/com/fixitb/Application.kt
@@ -10,7 +10,7 @@ fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
-
+//
 fun Application.module() {
     DatabaseFactory.init()
 
